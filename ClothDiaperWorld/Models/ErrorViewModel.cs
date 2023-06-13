9d81261471ad91f4a82715fdf44042266d341d32
Xayida83 <lotta.lindberg83@gmail.com>
2023-06-13 21:56:41 +0200

    First night

---
+++ ClothDiaperWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClothDiaperWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
